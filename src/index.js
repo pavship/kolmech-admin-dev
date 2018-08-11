892920dbbd7a9bd93f905c57f4a00a67f3ee41fc
Pavel Shipitsyn <pavship@yandex.ru>
2018-08-11 19:08:56 +0300

    connected graphQL data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({ uri: 'https://now-basic-njewgyhrkt.now.sh/' })
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+, document.getElementById('root'));
 registerServiceWorker();
