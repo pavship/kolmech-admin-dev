bb39900f006bb680c1b0f363a084d23ee7bf7b82
Pavel Shipitsyn <pavship@yandex.ru>
2019-06-07 18:58:06 +0300

    cleanup Porc.js

--- src/components/Deals/TableCells/Proc.js
+++ src/components/Deals/TableCells/Proc.js
@@ -1,70 +1,31 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Icon } from 'semantic-ui-react'
 
-import styled from 'styled-components'
 import { Div } from '../../styled/styled-semantic'
 import HtmlInput from '../../common/HtmlInput'
 import { assignNested } from '../../form/utils'
 
-const Input = styled.input`
-  width: 170px;
-`
-
 export default ({
-  deal,
   batch,
-  batchStructure,
   proc,
-  upsertDeal,
   upsertBatch
 }) => {
   const { id: procId} = proc
-  const { id: batchId, model: { id: modelId } } = batch
   const inputRef = useRef(null)
   const [ editMode, setEditMode ] = useState(false)
   useEffect(() => (editMode &&
     inputRef.current &&
     inputRef.current.focus()) || undefined)
-  const iniName = proc.name || ''
-  const [ name, setName ] = useState(iniName)
   if (editMode)
     return <HtmlInput
       ref={inputRef}
       placeholder='Новый техпроцесс'
-      value={name}
+      value={proc.name || ''}
       onChange={value => upsertBatch(draft => {
         assignNested(draft, 'procs[0].name', value)
         assignNested(draft, 'procs[0].modelId', batch.model.id, true)
       })}
     />
-    // return <Input
-    //   ref={inputRef}
-    //   placeholder='Новый техпроцесс'
-    //   value={name}
-    //   onChange={({ target: { value }}) => setName(value)}
-    //   onBlur={async () => {
-    //     if (name !== iniName && name !== '')
-    //       await upsertDeal({ variables: { input: {
-    //         id: deal.id,
-    //         batches: [
-    //           ...deal.batches.map(({ id }) => ({ id })).filter(b => b.id !== batchId),
-    //           {
-    //             id: batchId,
-    //             procs: [
-    //               ...batch.procs.map(({ id }) => ({ id })).filter(p => p.id !== procId),
-    //               {
-    //                 ...(procId !== 0)
-    //                   ? { id: procId }
-    //                   : { modelId },
-    //                 name
-    //               }
-    //             ]
-    //           }
-    //         ]
-    //       }}})
-    //     setEditMode(false)
-    //   }}
-    // />
   else if (procId === 0)
     return <Icon
       link
