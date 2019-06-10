5d4f274af9cfdc9f8825dac9ab227c43dbf47749
Pavel Shipitsyn <pavship@yandex.ru>
2019-06-10 23:28:59 +0300

    Update Batch.js

--- src/components/Deals/TableCells/Batch.js
+++ src/components/Deals/TableCells/Batch.js
@@ -9,8 +9,6 @@ import Model from './Model'
 import Qty from './Qty'
 import Procs from './Procs'
 import produce from 'immer';
-// import { useMutation } from 'react-apollo-hooks';
-import { Mutation } from 'react-apollo';
 
 const BatchContainer = styled.div`
   display: flex;
@@ -21,59 +19,42 @@ export default function Batch ({
   batch,
   upsertDeal,
 }) {
-  console.log('Batch > ')
   const [ upsertBatchProto ] = useMutation(uBq)
   const upsertBatch = draftHandler => upsertBatchProto({ variables: { input:
-    // console.log('produce(getStructure(batch), draftHandler) > ', produce(getStructure(batch), draftHandler)) || 
     produce(getStructure(batch), draftHandler)
   }})
-  // return <>
-  // <Mutation
-  //   mutation={uBq}
-  //   onError={() => console.log('onError > ')}
-  //   onCompleted={() => console.log('onCompleted > ')}
-  // >
-  //   {( upsertBatchProto ) => {
-  //     const upsertBatch = draftHandler => upsertBatchProto({ variables: { input:
-  //       // console.log('produce(getStructure(batch), draftHandler) > ', produce(getStructure(batch), draftHandler)) || 
-  //       produce(getStructure(batch), draftHandler)
-  //     }})
-      return <BatchContainer>
-        <Div
-          w='130px'
-          br={batch.id !== 0 ? '1px solid rgba(34,36,38,0.15);' : undefined}
-        >
-          <Model
-            deal={deal}
-            batch={batch}
-            upsertDeal={upsertDeal}
-          />
-        </Div>
-        {batch.id !== 0 && <>
-          <Div
-            w='40px;'
-          >
-            <Qty
-              deal={deal}
-              batchId={batch.id}
-              qty={batch.qty}
-              upsertDeal={upsertDeal}
-            />
-          </Div>
-          <Div
-            w='calc(170px+140px)'
-          >
-            <Procs
-              deal={deal}
-              batch={batch}
-              upsertDeal={upsertDeal}
-              upsertBatch={upsertBatch}
-            />
-          </Div>
-        </>}
-      </BatchContainer>
-  //   }}
-    
-  //   </Mutation>
-  // </>
+  return <BatchContainer>
+    <Div
+      w='130px'
+      br={batch.id !== 0 ? '1px solid rgba(34,36,38,0.15);' : undefined}
+    >
+      <Model
+        deal={deal}
+        batch={batch}
+        upsertDeal={upsertDeal}
+      />
+    </Div>
+    {batch.id !== 0 && <>
+      <Div
+        w='40px;'
+      >
+        <Qty
+          deal={deal}
+          batchId={batch.id}
+          qty={batch.qty}
+          upsertDeal={upsertDeal}
+        />
+      </Div>
+      <Div
+        w='calc(170px+140px)'
+      >
+        <Procs
+          deal={deal}
+          batch={batch}
+          upsertDeal={upsertDeal}
+          upsertBatch={upsertBatch}
+        />
+      </Div>
+    </>}
+  </BatchContainer>
 }
