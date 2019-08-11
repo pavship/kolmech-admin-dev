ef6ce1851e49a73eaf23c11af103d375e70fbf67
PS <pavship@yandex.ru>
2019-08-11 20:02:50 +0300

    + refreshToken in Root component
    tried to solve bug with graphql error when Anauthenticated

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import { useQuery } from './hooks/apolloHooks'
 import { me as meQuery } from '../graphql/user'
+import AuthContext from './auth/AuthContext'
 import UserContext from './context/UserContext'
 
 import EnquiriesPage from './Enquiry/EnquiriesPage'
@@ -10,36 +11,34 @@ import PaymentsPage from './Payments/Page'
 import DealsPage from './Deals/Page'
 
 export default function Root () {
+	const { refreshToken } = useContext(AuthContext)
 	const { data, loading, error } = useQuery(meQuery)
+	if (!loading && !error && !data) refreshToken(null)
 	const me = data && data.me
-	return <Router><>
+	return <>
 		{!error && loading ? 'Загрузка..' :
 			<UserContext.Provider
 				value={me}
 			>
-				<Switch>
-					<Route
-						exact
-						path="/"
-						render={() => (
-							<EnquiriesPage />
-						)}
-					/>
-					<Route
-						path="/pay"
-						render={() => (
-							<PaymentsPage />
-						)}
-					/>
-					<Route
-						path="/deals"
-						render={() => (
-							<DealsPage />
-						)}
-					/>
-					<Redirect to="/" />
-				</Switch>
+				<Router>
+					<Switch>
+						<Route
+							exact
+							path="/"
+							component={EnquiriesPage}
+						/>
+						<Route
+							path="/pay"
+							component={PaymentsPage}
+						/>
+						<Route
+							path="/deals"
+							component={DealsPage}
+						/>
+						<Redirect to="/" />
+					</Switch>
+				</Router>
 			</UserContext.Provider>
 		}
-	</></Router>
+	</>
 }
